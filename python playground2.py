f4cc67884aa141b726d43c523397517ebafd1b6f
DiyaRoseSabu <123435337+DiyaRoseSabu@users.noreply.github.com>
2023-05-24 10:32:05 +0530

    Add files via upload
    Basic Python programs

---
+++ python playground2.py
@@ -0,0 +1,60 @@
+class Person:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+    def greet(self):
+        print(f"Hello, my name is {self.name} and I am {self.age} years old.")
+
+
+person1 = Person("Alice", 25)
+
+
+print(person1.name)
+print(person1.age)
+
+
+person1.greet()
+
+
+
+#linkedlist
+
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+
+    def append(self, data):
+        new_node = Node(data)
+
+        if self.head is None:
+            self.head = new_node
+        else:
+            current = self.head
+            while current.next:
+                current = current.next
+            current.next = new_node
+
+    def display(self):
+        current = self.head
+        while current:
+            print(current.data, end=" ")
+            current = current.next
+        print()
+
+
+my_list = LinkedList()
+
+
+my_list.append(10)
+my_list.append(20)
+my_list.append(30)
+my_list.append(40)
+
+# Display
+my_list.display()
